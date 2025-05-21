93163e5de05f6c456df1ea6e8bb800dcba42a499
Michailidis-Stavros <stmi2003@gmail.com>
2025-05-21 12:21:27 +0300

    defined run function for spot_goal subcommand

---
+++ src/cli/spot_goal.py
@@ -0,0 +1,16 @@
+import json
+
+from src.core.goal_node.goal_spotter import iterate, execute
+from src.utils.pdf_to_json import pdftojson
+
+from langchain_community.tools import ReadFileTool, WriteFileTool
+
+def run(pdf_path):
+    make_to_json = pdftojson (pdf_path)
+
+    toolbox = [ReadFileTool (verbose=True), WriteFileTool (verbose=True)]
+    program = iterate (toolbox)
+    result = execute (program, json.dumps (make_to_json, indent=2, ensure_ascii=False))
+
+    with open ("goal.txt", "w") as f:
+        f.write (result)
