86b9714b719fddff8226b4bde25d343640592960
Michailidis-Stavros <stmi2003@gmail.com>
2025-06-21 10:52:42 +0300

    fixed graph bug in implementation workflow, added debugging prints and renamed files and dir

--- src/cli/implement.py
+++ src/cli/implement.py
@@ -1,6 +1,5 @@
 from langchain_community.tools import ReadFileTool, WriteFileTool
-from src.core.implementation_node.implementer import iterate, execute
-
+from src.core.implement.implementer import iterate, execute
 
 def run_impl (goal):
     toolbox = [ReadFileTool (verbose=True), WriteFileTool (verbose=True)]
--- src/core/implement/implementer.py
+++ src/core/implement/implementer.py
@@ -11,53 +11,68 @@ from langgraph.prebuilt import ToolNode
 from langgraph.checkpoint.memory import MemorySaver
 from functools import partial
 
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
 NEUROSYM_DEFAULT_MODEL = os.environ.get ("NEUROSYM_DEFAULT_MODEL", "gpt-4o-mini")
 
 def should_continue (state):
     messages = state ["messages"]
     last_message = messages [-1]
+    print("==> LLM Response:", last_message.content)
+    print("==> Tool Calls:\n", last_message.tool_calls)
 
     if not last_message.tool_calls:
+        print ("No tools called.")
         return "end"
     else:
         return "continue"
 
-# this can def be improved
 SYSTEM_PROMPT = os.environ.get ("SYSTEM_PROMPT", """
-                                You're a LLM agent node in a workflow meant to implement software for the task provided. 
-                                You'll be given tools to write code and read what you wrote to make corrections. 
+                                You're a LLM agent node in a workflow meant to implement software for the task provided.
+                                _Provide code_. You'll be given tools to write code and read what you wrote to make corrections. 
                                 Your code will be given to a validator who will automatically test what you wrote and 
                                 if any test case returns an error you'll be required to re-write the implementation.
                                 Make sure any inputs expected will be given via argv. _Not_ scanf.
                                 """)
 
-def call_model (state, config, model):
+def call_model (state, config, model, prompt):
     messages = state ["messages"]
-    messages = [{"role": "system", "content": SYSTEM_PROMPT}] + messages
+    messages = [SystemMessage (prompt)] + messages
     response = model.invoke (messages)
 
     return {"messages": [response]}
 
 def iterate (toolbox):
     workflow = StateGraph (AgentState, GraphConfig)
+    
+    # init model, define toolnode and bind tools
     tool_node = ToolNode (toolbox)
-
     model = ChatOpenAI (temperature=0.5, model=NEUROSYM_DEFAULT_MODEL)
     model = model.bind_tools (toolbox)
 
-    workflow.add_node ("implementer", partial (call_model, model=model))
-    workflow.set_entry_point ("implementer")
-
+    # node definitions
+    workflow.add_node ("summarizer", partial (call_model, model=model, prompt=SYSTEM_PROMPT))
     workflow.add_node ("action", tool_node)
-    workflow.add_conditional_edges ("implementer", should_continue, {"continue": "action", "end": END,},)
 
+    # set graph's entry point
+    workflow.set_entry_point ("summarizer")
+
+    # define graph's edges
+    workflow.add_conditional_edges ("summarizer", should_continue, {"continue": "action", "end": END,},)
+    workflow.add_edge ("action", "summarizer")
+
+    # model has memory
     checkpointer = MemorySaver ()
+    # compile graph
     graph = workflow.compile (checkpointer=checkpointer)
+    # illustrate graph for debugging
+    img = graph.get_graph().draw_mermaid_png()
+    with open("graph.png", "wb") as f:
+        f.write(img)
+        #TODO inject file name for source code and print it
+        print("Graph written to graph.png")
     return graph
 
-# got no clue what some of these mean in a deep sense
 def execute (program, user_in:str) -> str:
     final_state = program.invoke ({"messages": [HumanMessage (content=user_in)]}, config= {"configurable": {"thread_id": 24}, "recursion_limit": 100})
     return final_state ["messages"][-1].content
