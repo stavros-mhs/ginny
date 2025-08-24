c8ba35dcf07788545cf8a16f82d02f7159a26b3e
Michailidis-Stavros <stmi2003@gmail.com>
2025-08-25 00:39:51 +0300

    data passed to get_tests  wrapper are now the extracted text and not the summary of user_in.pdf

--- src/core/solve/agent_logic/get_tests/get_tests.py
+++ src/core/solve/agent_logic/get_tests/get_tests.py
@@ -4,11 +4,11 @@ from src.core.solve.agent_logic.custom_types.ctypes import TestCaseList
 from src.utils.pretty_print import beautify
 
 def get_tests_wrapper (state: AgentState, model, token_logger):
-    assignment_summary = state ["assignment_summary"]
+    text = state ["extracted_text"]
     agent = build_agent (model=model, token_logger=token_logger)
     chain = cast_chain (llm=agent, type=TestCaseList)
 
-    chain_out = chain.invoke ({"data": assignment_summary, "type": TestCaseList})
+    chain_out = chain.invoke ({"data": text, "type": TestCaseList})
     #print (f"type of chain: {type(chain_out)}")
     unwrapped_chain = chain_out[0]
     #print (f"type of unwrapped chain: {type (unwrapped_chain)}")
