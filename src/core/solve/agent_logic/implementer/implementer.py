a1a2395cdc80987fe2c86462b3a030e5ac3ce369
Michailidis-Stavros <stmi2003@gmail.com>
2025-08-25 00:17:55 +0300

    moved implementer logic to separate dir

---
+++ src/core/solve/agent_logic/implementer/implementer.py
@@ -0,0 +1,21 @@
+from src.utils.state import AgentState
+from src.core.solve.agent_logic.sys_prompts import IMPLEMENTER_SYSTEM_PROMPT
+from src.core.solve.agent_logic.generics import build_agent
+
+from langchain_community.tools import ReadFileTool, WriteFileTool
+
+def implementer_wrapper (model, token_logger):
+    implementer = build_agent (
+        temperature=0.8,
+        model=model,
+        token_logger=token_logger
+    )
+    
+    toolbox = [
+        ReadFileTool(root_dir="working_dir"),
+        WriteFileTool(root_dir="working_dir")
+    ]
+
+    implementer = implementer.bind_tools (toolbox)
+
+    return implementer
