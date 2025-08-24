acfe4b8b4234cccc07ecbb806ee54ab75f94f905
Michailidis-Stavros <stmi2003@gmail.com>
2025-08-25 00:19:11 +0300

    moved summarizer logic to separate folder

--- src/core/solve/agent_logic/summarizer/summarizer.py
+++ src/core/solve/agent_logic/summarizer/summarizer.py
@@ -2,12 +2,13 @@ from src.utils.state import AgentState
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage
 
-from src.core.solve.agent_logic.sys_prompts import SUMMARIZER_SYSTEM_PROMPT, NEUROSYM_DEFAULT_MODEL
+from src.core.solve.agent_logic.sys_prompts import SUMMARIZER_SYSTEM_PROMPT
 from src.utils.pretty_print import beautify
+from src.core.solve.agent_logic.generics import build_agent
 
-def get_summary(state: AgentState, token_logger):
+def get_summary(state: AgentState, model, token_logger):
     extracted = state["extracted_text"]
-    summarizer = ChatOpenAI(temperature=0.5, model=NEUROSYM_DEFAULT_MODEL, timeout=10, callbacks=[token_logger])
+    summarizer = build_agent (temperature=0.5, model=model, token_logger=token_logger)
 
     messages = [
         SystemMessage(content=SUMMARIZER_SYSTEM_PROMPT),
