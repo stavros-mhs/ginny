a841db0116bad6413c5ce45390da9f002fafe0bf
Michailidis-Stavros <stmi2003@gmail.com>
2025-10-17 09:47:53 +0300

    gave ls access to implementer agent

--- src/core/solve/agent_logic/implementer/implementer.py
+++ src/core/solve/agent_logic/implementer/implementer.py
@@ -2,7 +2,7 @@ from src.utils.state import AgentState
 from src.core.solve.agent_logic.sys_prompts import IMPLEMENTER_SYSTEM_PROMPT
 from src.core.solve.agent_logic.generics import build_agent
 
-from langchain_community.tools import ReadFileTool, WriteFileTool
+from langchain_community.tools import ReadFileTool, WriteFileTool, ListDirectoryTool
 
 def implementer_wrapper (model, token_logger):
     implementer = build_agent (
@@ -13,7 +13,8 @@ def implementer_wrapper (model, token_logger):
     
     toolbox = [
         ReadFileTool(root_dir="working_dir"),
-        WriteFileTool(root_dir="working_dir")
+        WriteFileTool(root_dir="working_dir"),
+        ListDirectoryTool (root_dir="working_dir")
     ]
 
     implementer = implementer.bind_tools (toolbox)
